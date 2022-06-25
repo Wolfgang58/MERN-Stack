89a192b6f12c461c766587d99a56ea11ba8626e7
Veli KURT <veli.kurt.9041@hotmail.com>
2022-06-25 23:36:53 +0300

    router

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ require('dotenv').config()
 const express=require('express')
 
 const app=express()
+const workoutRoutes=require('./routes/workouts')
+
+app.use(express.json())
 
 app.use((req, res, next) =>{
   console.log(req.path, req.method)
   next()
 })
 
-app.get('/',(req, res) =>{
-  res.json({mssg:'Welcome to the app'})
-})
+app.use('/api/workouts/',workoutRoutes)
+
 
 app.listen(process.env.PORT, ()=>{
   console.log('Server running at http://localhost:4000',process.env.PORT)
