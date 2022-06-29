b336c8b53ff9d20ec80b8609152e8b20caf92847
Veli KURT <veli.kurt.9041@hotmail.com>
2022-06-29 23:30:02 +0300

    controller

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,29 +1,20 @@
 const express = require('express')
-const Workout = require('../models/workoutModel')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout,
+} = require('../controller/workoutController')
 
 const router=express.Router()
 
 //GET all workouts
-router.get('/', (req,res) => {
-    res.json({mssg:'GET all workouts'})
-})
+router.get('/', getWorkouts)
 
 //GET one workout
-router.get('/:id',(req,res) => {
-    res.json({mssg:'GET a single workout'})
-})
+router.get('/:id',getWorkout)
 
 //POST a workout
-router.post('/',async(req,res) => {
-    const{title,reps,load}=req.body
-
-    try{
-        const workout=await Workout.create({title,load,reps})
-        res.status(200).json(workout)
-    }catch(error){
-        res.status(400).json({error:error.message})
-    }
-})
+router.post('/', createWorkout)
 
 //DELETE a workout
 router.delete('/:id',(req,res) => {
